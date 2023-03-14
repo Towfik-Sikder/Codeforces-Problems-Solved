5de5c601403ece32eda735a72dad4ffb6f6245ce
Md Yeasin Arafat <116306992+mdyeasinaraf07@users.noreply.github.com>
2023-03-14 20:54:08 +0600

    Add files via upload

---
+++ codeforces/CF-Square-Counting.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int t;
+    cin>> t;
+    while(t--){
+        long long int n;
+        cin>> n;
+        long long int s;
+        cin>> s;
+        n = n * n;
+        cout<< s / n << "\n";
+    }
+
+
+    return 0;
+}
